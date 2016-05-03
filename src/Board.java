1be396b25dc556f4c85b40f745077419f5439108
William Yznaga <wyznaga@gmail.com>
2016-05-03 17:03:37 +0000

    Created Board class with board of '_'s

---
+++ src/Board.java
@@ -0,0 +1,12 @@
+public class Board
+{
+    private char[][] board = {{'_','_','_'},{'_','_','_'},{'_','_','_'}};
+    public char[][] getBoard()
+    {
+        return this.board;
+    }
+    public void setBoard(char[][] boardIn)
+    {
+        this.board = boardIn;
+    }
+}
